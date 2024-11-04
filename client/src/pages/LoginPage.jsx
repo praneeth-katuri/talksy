bc9073a64b56cef57212c38f88227c379b61fa46
praneeth-katuri <praneethkaturi@outlook.com>
2024-11-04 13:31:19 +0530

    refactor: Refactor login page to use zod validation and react-hook-form

--- client/src/pages/LoginPage.jsx
+++ client/src/pages/LoginPage.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+
 import { Card, CardContent } from "@/components/ui/card";
 import { Label } from "@/components/ui/label";
 import { Separator } from "@/components/ui/separator";
@@ -6,14 +7,34 @@ import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Link } from "react-router-dom";
 
+import { Eye, EyeOff } from "lucide-react";
+
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+
+const loginSchema = z.object({
+  email: z
+    .string()
+    .min(1, "Email Required")
+    .email("Enter a valid email address."),
+  password: z
+    .string()
+    .min(1, "Password Required")
+    .min(6, "Password must be atleast 6 characters"),
+});
+
 const LoginPage = () => {
-  const [userName, setUserName] = useState("");
-  const [password, setPassword] = useState("");
+  const [showPassword, setShowPassword] = useState(false);
 
-  const handleLogin = (e) => {
-    e.preventDefault();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, dirtyFields, submitCount },
+  } = useForm({ resolver: zodResolver(loginSchema) });
 
-    console.log("Welcome");
+  const handleLogin = (data) => {
+    console.log("Welcome", data);
   };
 
   return (
@@ -23,28 +44,49 @@ const LoginPage = () => {
           <h2 className="text-2xl font-semibold mb-6 text-center">
             Login to Talksy..
           </h2>
-          <form onSubmit={handleLogin}>
+          <form onSubmit={handleSubmit(handleLogin)}>
             <div className="mb-4">
               <Input
                 className="py-6 md:text-base rounded"
                 id="email"
-                type="email"
-                value={userName}
+                type="text"
                 placeholder="Email address"
-                onChange={(e) => setUserName(e.target.value)}
-                required
+                {...register("email")}
               />
+              {dirtyFields.email || submitCount > 0
+                ? errors.email && (
+                    <p className="text-red-600 text-sm mt-1">
+                      {errors.email.message}
+                    </p>
+                  )
+                : null}
             </div>
-            <div>
-              <Input
-                className="rounded mb-4 py-6 md:text-base"
-                id="password"
-                type="password"
-                placeholder="Password"
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
-                required
-              />
+
+            <div className="mb-4">
+              <div className="relative">
+                <Input
+                  className="rounded py-6 md:text-base"
+                  id="password"
+                  type={showPassword ? "text" : "password"}
+                  placeholder="Password"
+                  {...register("password")}
+                />
+                <button
+                  type="button"
+                  className="absolute right-3 top-1/2 transform -translate-y-1/2 text-gray-400 hover:text-gray-500"
+                  onClick={() => setShowPassword((prev) => !prev)}
+                  tabIndex={-1}
+                >
+                  {showPassword ? <EyeOff size={20} /> : <Eye size={20} />}
+                </button>
+              </div>
+              {dirtyFields.password || submitCount > 0
+                ? errors.password && (
+                    <p className="text-red-600 text-sm mt-1">
+                      {errors.password.message}
+                    </p>
+                  )
+                : null}
             </div>
             <Button
               type="submit"
