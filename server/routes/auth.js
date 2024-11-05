8a39d3281720db3d5a2f9ad7b2d542d78b39e5d9
praneeth-katuri <praneethkaturi@outlook.com>
2024-11-05 12:11:12 +0530

    feat(routes): Added routes for Login and SignUp

---
+++ server/routes/auth.js
@@ -0,0 +1,47 @@
+const express = require("express");
+const jwt = require("jsonwebtoken");
+const User = require("../models/User");
+const router = express.router();
+
+router.post("/signup", async (req, res) => {
+  try {
+    const { username, email, password } = req.body;
+
+    const userExists = await User.findOne({ user });
+
+    if (userExists) {
+      return res.status(400).json({ message: "User already exists" });
+    }
+    const user = new User({ username, email, password });
+    await user.save();
+
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
+      expiresIn: "1d",
+    });
+
+    res.status(201).json({ token, user: { id: user._id, username, email } });
+  } catch (err) {
+    res.status(500).json({ message: "Server error" });
+  }
+});
+
+router.post("/login", async (req, res) => {
+  try {
+    const { username, password } = req.body;
+
+    const user = await User.findOne({ username });
+    if (!user) {
+      return res.status(400).json({ message: "Invalid Credentials" });
+    }
+    const isMatch = await user.comparePassword(password);
+    if (!isMatch) {
+      return res.status(400).json({ message: "Invalid Credentials" });
+    }
+
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
+      expiresIn: "1d",
+    });
+  } catch (err) {
+    res.status(500).json({ message: "Server Error" });
+  }
+});
