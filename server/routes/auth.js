3d4c4d336ca5b33e14f518de1cce4d26ef098e94
praneeth-katuri <praneethkaturi@outlook.com>
2024-11-06 04:12:22 +0530

    fix: fixed errors in auth route

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,13 +1,13 @@
 const express = require("express");
 const jwt = require("jsonwebtoken");
 const User = require("../models/User");
-const router = express.router();
+const router = express.Router();
 
 router.post("/signup", async (req, res) => {
   try {
     const { username, email, password } = req.body;
 
-    const userExists = await User.findOne({ user });
+    const userExists = await User.findOne({ email });
 
     if (userExists) {
       return res.status(400).json({ message: "User already exists" });
@@ -21,6 +21,7 @@ router.post("/signup", async (req, res) => {
 
     res.status(201).json({ token, user: { id: user._id, username, email } });
   } catch (err) {
+    console.error(err)
     res.status(500).json({ message: "Server error" });
   }
 });
@@ -41,7 +42,14 @@ router.post("/login", async (req, res) => {
     const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
       expiresIn: "1d",
     });
+    res.json({
+      token,
+      user: { id: user._id, username, email: user.email },
+    });
   } catch (err) {
+    console.error(err)
     res.status(500).json({ message: "Server Error" });
   }
 });
+
+module.exports = router;
