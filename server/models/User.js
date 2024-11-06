9db8f5cdccf1781d5c8ecb3c72dc83adc41707d9
praneeth-katuri <praneethkaturi@outlook.com>
2024-11-06 14:00:00 +0530

    refactor(models/user): Refactor User model

--- server/models/User.js
+++ server/models/User.js
@@ -1,21 +1,25 @@
 const mongoose = require("mongoose");
 const bcrypt = require("bcryptjs");
 
-const userSchema = new mongoose.Schema({
-  username: { type: String, required: true, unique: true },
-  email: { type: String, required: true, unique: true },
-  password: { type: String, required: true, select: false },
-  profilePicture: {
-    type: String,
-    default:
-      "https://res.cloudinary.com/dpquxv6mk/image/upload/v1745630258/default-avatar_hfyuwp.jpg",
+const userSchema = new mongoose.Schema(
+  {
+    username: { type: String, required: true, unique: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true, select: false },
+    profilePicture: {
+      type: String,
+      default:
+        "https://res.cloudinary.com/dpquxv6mk/image/upload/v1745630258/default-avatar_hfyuwp.jpg",
+    },
+    status: { type: String, enum: ["online", "offline"], default: "offline" },
+    lastSeen: { type: Date, default: Date.now },
+    friends: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
+    friendRequests: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
   },
-  status: { type: String, enum: ["online", "offline"], default: "offline" },
-  lastSeen: { type: Date, default: Date.now },
-  friends: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
-  friendRequests: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
-  createdAt: { type: Date, default: Date.now },
-});
+  {
+    timestamps: true,
+  }
+);
 
 userSchema.pre("save", async function (next) {
   if (!this.isModified("password")) next();
