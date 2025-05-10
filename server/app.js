f144baaecd3115ed2985560751fa6eb4f95a57d7
praneeth-katuri <praneethkaturi@outlook.com>
2025-05-10 09:27:58 +0530

    refactor: Refactor app.js

--- server/app.js
+++ server/app.js
@@ -1,9 +1,17 @@
 const express = require("express");
 const connectDB = require("./config/db");
+const cors = require("cors");
+const authRoutes = require("./routes/auth");
+const app = express();
 require("dotenv").config();
 
-const app = express();
+app.use(express.json());
+app.use(cors());
 
+app.use("/api/auth", authRoutes);
+
+app.use("/api/users", require("./routes/users"));
+app.use("/api/frineds", require("./routes/f"))
 connectDB();
 
 module.exports = app;
